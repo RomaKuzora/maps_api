1df095a9c851174ae5b279a35242216ded649810
Роман Кузора <romakuzora@mail.ru>
2024-02-02 23:28:39 +0300

    начало 4 задания

--- main.py
+++ main.py
@@ -8,41 +8,51 @@ from PyQt6.QtCore import Qt
 from PIL import Image, ImageQt
 from io import BytesIO
 
-lat = 39.558881  # широта
-lon = 50.199912  # долгота
-z = 19
-x = None
-
-
-# 0.001 min
-# 75 max
-
 
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.initUi()
+
+    def initUi(self):
+        self.type_map = 'map'
+        self.lon = 39.558881  # широта
+        self.lat = 50.199912  # долгота
+        self.z = 19
+        self.get_image_map()
+        for radio_button in self.map_group.buttons():
+            radio_button.toggled.connect(self.set_map)
+
+    def set_map(self):
+        sender = self.sender().text()
+        if sender == 'карта':
+            self.type_map = 'map'
+        elif sender == 'гибрид':
+            self.type_map = 'sat,skl'
+        elif sender == 'спутник':
+            self.type_map = 'sat'
         self.get_image_map()
 
     def get_image_map(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={lat},{lon}&z={z}&l=map"
+        map_request = f"https://static-maps.yandex.ru/1.x/?ll={self.lon},{self.lat}&l={self.type_map}&z=17"
         response = requests.get(map_request)
         self.map_label.setPixmap(QPixmap.fromImage(ImageQt.ImageQt(Image.open(BytesIO(response.content)))))
 
     def keyPressEvent(self, event):
-        global z, lat, lon
-        if event.key() == Qt.Key.Key_PageUp and z < 19:
-            z += 1
-        if event.key() == Qt.Key.Key_PageDown and z > 0:
-            z -= 1
-        if event.key() == Qt.Key.Key_Left:
-            lat -= 2 / (z ** 2)
-        if event.key() == Qt.Key.Key_Right:
-            lat += 2 / (z ** 2)
-        if event.key() == Qt.Key.Key_Up:
-            lon += 2 / (z ** 2)
-        if event.key() == Qt.Key.Key_Down:
-            lon -= 2 / (z ** 2)
+        if event.key() == Qt.Key.Key_PageUp and self.z < 19:
+            self.z += 1
+        if event.key() == Qt.Key.Key_PageDown and self.z > 0:
+            self.z -= 1
+        if self.z != 0:
+            if event.key() == Qt.Key.Key_Left:
+                self.lat -= 2 / (self.z ** 2)
+            if event.key() == Qt.Key.Key_Right:
+                self.lat += 2 / (self.z ** 2)
+            if event.key() == Qt.Key.Key_Up:
+                self.lon += 2 / (self.z ** 2)
+            if event.key() == Qt.Key.Key_Down:
+                self.lon -= 2 / (self.z ** 2)
         self.get_image_map()
 
 
